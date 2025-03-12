cbb9fbdc36ede467980480cdba6c3a2f97622e1a
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-03-12 11:22:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/howto_open_short_link
 
 # Shortner 
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bedlio.xyz')
+SHORTLINK_API = environ.get('SHORTLINK_API', '48b4aed300d4a3b600da572df5c416c25cd22da1')
 TUTORIAL = environ.get('TUTORIAL', '') # Tutorial video link for opening shortlink website 
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
