3300bd7eb68614caf74398eb049e0e411ef25021
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-03-13 22:23:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002327190146')) #Log channel id 
 DEENDAYAL_MOVIE_UPDATE_CHANNEL = int(environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL', '-1002267482168')) #Notification of those who verify will be sent to your channel. Enter the ID of the channel you want to send notification to here.
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002315755353')) # add premium logs channel id
 auth_channel = environ.get('AUTH_CHANNEL', '-1002267482168') #Channel / Group Id for force sub ( make sure bot is admin )
-DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002206211693').split()]
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002476939852').split()]
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '0') # support group id ( make sure bot is admin )
 reqst_channel = environ.get('REQST_CHANNEL_ID', '0') # request channel id ( make sure bot is admin ).
 
