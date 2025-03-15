7a7184d6b570b9180a355a0c522f69d41e2cdc25
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-03-15 15:05:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( False ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( False ) / Off ( False )
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 4)) # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1002502768416')) #Log channel id ( make sure bot is admin )
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/howto_open_short_link/12') # How to open tutorial link for verification
