119b3cf4764bcb78b0c390082321012ee9e5619c
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-07-20 22:08:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,13 +54,13 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 4)) # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1002502768416')) #Log channel id ( make sure bot is admin )
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/howto_open_short_link/12') # How to open tutorial link for verification
 
 # Shortner 
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bedlio.xyz')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'c605be34bcdc02bfed6e64910437b40be91d5a95')
 TUTORIAL = environ.get('TUTORIAL', '') # Tutorial video link for opening shortlink website 
